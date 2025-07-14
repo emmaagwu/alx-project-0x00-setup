8b55ddcfe1f1bb3ab2e804cab6886583fc0cd649
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-14 21:46:20 +0100

    Add About and Landing pages components

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,12 @@
+const About: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-4xl font-semibold">About Page</h1>
+      <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+        Learn More
+      </button>
+    </main>
+  );
+}
+
+export default About;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,12 @@
+const Landing: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center">
+      <h1 className="text-4xl font-semibold">Landing Page</h1>
+      <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+        Get Started
+      </button>
+    </main>
+  );
+}
+
+export default Landing;
