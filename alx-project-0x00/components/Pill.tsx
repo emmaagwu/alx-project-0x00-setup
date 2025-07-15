bb5a4ca6f98522a1f6a446ec647fea487293da30
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-07-15 18:00:51 +0100

    Refactor Pill component to accept title as a prop and add PillProps interface

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,9 @@
-const Pill: React.FC = (title: string) => {
+import { PillProps } from "@/interfaces";
+
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
